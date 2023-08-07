aef10cf051d0e665ac1b64dac8abe3d29feb0fe1
Cristhian <cristhianverarivas22@gmail.com>
2023-08-06 22:53:07 -0500

    first commit

---
+++ src/components/Integrantes/Integrantes.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import NavBar from "../NavBar/NavBar";
+
+const Integrantes = () => {
+return(
+    <>
+    <NavBar/>
+    <div>
+        <h2 className="text-center">Integrantes</h2>
+        <ul>
+            <li>Rodriguez Peova Angie Nicole</li>
+            <li>Rodriguez Soldispa Nixon Jair</li>
+            <li>Juan Diego Andrade Calero</li>
+        </ul>
+    </div>
+    </>
+)
+}
+
+export default Integrantes
